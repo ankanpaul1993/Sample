d13cc828e20bdd52beea72b9a1536f8fbc7879cb
ankanpaul1993 <46998752+ankanpaul1993@users.noreply.github.com>
2019-06-03 15:51:01 +0530

    Update hello.java

--- src/test/java/hello.java
+++ src/test/java/hello.java
@@ -6,6 +6,18 @@ public class hello {
 	public void abc() {
 		// TODO Auto-generated method stub
 		
+	System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+ 
+// Initialize browser
+WebDriver driver=new ChromeDriver();
+ 
+// Open facebook
+driver.get("http://www.facebook.com");
+ 
+// Maximize browser
+ 
+driver.manage().window().maximize();
+		
 		System.out.println("Hello");
 		
 
